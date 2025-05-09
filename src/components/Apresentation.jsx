af5ce202e3287825988f23c31205534950cc3fc7
ThiSiniero <thiagosiniero@gmail.com>
2025-05-09 16:37:11 -0300

    make the apresentation section

--- src/components/Apresentation.jsx
+++ src/components/Apresentation.jsx
@@ -3,24 +3,22 @@ import fundoHome from '../imgs/fundo-home.jpg';
 
 const Apresentation = () => {
     return (
-    
-    
-        <div style={{ backgroundImage: `url(${fundoHome})` }} className="bg-cover bg-center min-h-screen flex flex-col justify-center items-center text-center px-4 pt-28 md:pt-16">
-            <div className="max-w-[80%] w-full bg-gray-900 bg-opacity-90 p-8 rounded-lg shadow-lg">
-                <p className="text-2xl md:text-xl font-semibold text-white mb-6">Olá! Meu nome é Thiago Siniero, </p>
-                <p className="text-2xl md:text-xl font-semibold text-white mb-4">
-                    Sou um desenvolvedor full-stack e atualmente estou cursando Engenharia da Computação na Universidade São Francisco (USF). 
+        <div style={{ backgroundImage: `url(${fundoHome})` }} className="bg-cover bg-center min-h-screen flex flex-col justify-center items-center text-center px-4 pt-28 md:pt-0">
+            <p className='text-white text-[350%] font-light tracking-wider mb-6'>Thiago Siniero</p>
+            <div className="max-w-[80%] w-full p-8 rounded-lg shadow-lg mt-20">
+                <p className="text-2xl md:text-xl font-white text-white mb-4">
+                    Olá! Sou um desenvolvedor full-stack e atualmente estou cursando Engenharia da Computação na Universidade São Francisco (USF). 
                     Ao longo da minha trajetória, venho desenvolvendo projetos completos, integrando front-end e back-end, com foco em qualidade, boas práticas e experiência do usuário.
                 </p>
-                <p className="text-lg md:text-xl font-semibold text-white mb-4">
+                <p className="text-lg md:text-xl font-white text-white mb-4">
                     Atualmente, estou em busca de crescimento profissional, ampliando meus conhecimentos em tecnologias modernas como React, Node.js, TypeScript e Docker.
                     Além dos estudos, dedico meu tempo ao desenvolvimento de projetos próprios e construção de um portfólio que reflita minhas habilidades e minha evolução como desenvolvedor.
                 </p>
-                <p className="text-lg md:text-xl font-semibold text-white">
+                <p className="text-lg md:text-xl font-white text-white">
                     Meu objetivo é atuar em projetos desafiadores, colaborar com times de alta performance e continuar aprendendo todos os dias. Se quiser trocar uma ideia, venha conferir meus projetos ou trabalhar comigo, será um prazer conversar!
                 </p>
             </div>
-    </div>
+        </div>
     );
 };
 
--- src/components/Formação.jsx
+++ src/components/Formação.jsx
@@ -9,7 +9,7 @@ import SkillLab from '../imgs/SkillLab.jpg';
 const Formação = () => {
 
     const bigImg = (img) => {
-        img.style.transform = "scale(1.5)";
+        img.style.transform = "scale(1.3)";
         img.style.transition = "transform 0.25s ease";
     };
 
@@ -19,91 +19,55 @@ const Formação = () => {
     };
 
     return (
-        <main className="flex flex-col items-center w-full" id="certificados">
-            <h3 className="w-full text-center text-[250%] font-bold mb-20">Formação Acadêmica</h3>
+        <main className="flex flex-col items-center justify-center w-full min-h-screen bg-black text-white" id="certificados">
+            <h3 className="w-full text-center text-[250%] font-bold mb-36">Formação Acadêmica</h3>
 
-            <div className="flex flex-col items-center mb-8">
-                <img
-                    onMouseEnter={(e) => bigImg(e.target)}
-                    onMouseOut={(e) => normalImg(e.target)}
-                    className="w-1/4 mb-4 cursor-pointer transition-transform"
-                    src={Dio1}	
-                    alt=""
-                />
-                <p className="text-center text-base">
+            <div className="flex flex-col md:flex-row items-center justify-center mb-6">
+                <img onMouseEnter={(e) => bigImg(e.target)} onMouseOut={(e) => normalImg(e.target)} className="w-3/4 md:w-1/4 mb-4 md:mb-0 md:mr-48 cursor-pointer transition-transform" src={Dio1} alt=""/>
+                <p className="text-center md:text-left text-base w-full md:w-1/3">
                     Este foi o meu primeiro certificado, concluído no site da{' '}
-                    <a href="https://web.digitalinnovation.one" className="text-blue-500 underline">digitalinnovation.one</a>, <br /> onde aprendi as regras básicas da programação e algumas palavras <br /> essenciais como: 'software', 'hardware', 'framework', 'back-end', <br /> 'front-end', 'fullstack', entre outras.
+                    <a href="https://web.digitalinnovation.one" className="text-blue-700 underline">digitalinnovation.one</a>, onde aprendi as regras básicas da programação e algumas palavras essenciais como: 'software', 'hardware', 'framework', 'back-end', 'front-end', 'fullstack', entre outras.
                 </p>
             </div>
-            <hr className="w-full border-t border-gray-300 mb-8" />
+            <hr className="w-full border-t border-gray-700 mb-6" />
 
-            <div className="flex flex-col items-center mb-8">
-                <img
-                    onMouseEnter={(e) => bigImg(e.target)}
-                    onMouseOut={(e) => normalImg(e.target)}
-                    className="w-1/4 mb-4 cursor-pointer transition-transform"
-                    src={Dio2}
-                    alt=""
-                />
-                <p className="text-center text-base">
+            <div className="flex flex-col md:flex-row items-center justify-center mb-6">
+                <img onMouseEnter={(e) => bigImg(e.target)} onMouseOut={(e) => normalImg(e.target)} className="w-3/4 md:w-1/4 mb-4 md:mb-0 md:mr-48 cursor-pointer transition-transform" src={Dio2} alt=""/>
+                <p className="text-center md:text-left text-base w-full md:w-1/3">
                     Este foi o curso, também feito na{' '}
-                    <a href="https://web.digitalinnovation.one" className="text-blue-500 underline">digitalinnovation.one</a> <br /> onde comecei a aprender HTML e CSS, montando meus primeiros layouts <br /> e entendendo ainda mais sobre como funciona a codificação e como <br /> isso é transformado em informações da página.
+                    <a href="https://web.digitalinnovation.one" className="text-blue-700 underline">digitalinnovation.one</a> onde comecei a aprender HTML e CSS, montando meus primeiros layouts e entendendo ainda mais sobre como funciona a codificação e como isso é transformado em informações da página.
                 </p>
             </div>
-            <hr className="w-full border-t border-gray-300 mb-8" />
+            <hr className="w-full border-t border-gray-700 mb-6" />
 
-            <div className="flex flex-col items-center mb-8">
-                <img
-                    onMouseEnter={(e) => bigImg(e.target)}
-                    onMouseOut={(e) => normalImg(e.target)}
-                    className="w-1/4 mb-4 cursor-pointer transition-transform"
-                    src={Dio3}
-                    alt=""
-                />
-                <p className="text-center text-base">
-                    Neste curso comecei a me aprofundar mais no CSS e utilizei o meu primeiro <br /> framework, o <a href="https://getbootstrap.com/" className="text-blue-500 underline">Bootstrap</a>, onde aprendi muitas facilidades, economizando <br /> muito tempo para fazer algumas codificações, <br /> ou seja, um trabalho mais ágil.
+            <div className="flex flex-col md:flex-row items-center justify-center mb-6">
+                <img onMouseEnter={(e) => bigImg(e.target)} onMouseOut={(e) => normalImg(e.target)} className="w-3/4 md:w-1/4 mb-4 md:mb-0 md:mr-48 cursor-pointer transition-transform" src={Dio3} alt=""/>
+                <p className="text-center md:text-left text-base w-full md:w-1/3">
+                    Neste curso comecei a me aprofundar mais no CSS e utilizei o meu primeiro framework, o <a href="https://getbootstrap.com/" className="text-blue-700 underline">Bootstrap</a>, onde aprendi muitas facilidades, economizando muito tempo para fazer algumas codificações, ou seja, um trabalho mais ágil.
                 </p>
             </div>
-            <hr className="w-full border-t border-gray-300 mb-8" />
+            <hr className="w-full border-t border-gray-700 mb-6" />
 
-            <div className="flex flex-col items-center mb-8">
-                <img
-                    onMouseEnter={(e) => bigImg(e.target)}
-                    onMouseOut={(e) => normalImg(e.target)}
-                    className="w-1/4 mb-4 cursor-pointer transition-transform"
-                    src={Dio4}
-                    alt=""
-                />
-                <p className="text-center text-base">
-                    Este certificado representa minha competência com o git e github, <br /> onde aprendi a trabalhar em equipe, criar um repositório <br /> pessoal ou coletivo, fazer commits, subir meus códigos, clonar trabalhos e <br /> resolver conflitos de merge.
+            <div className="flex flex-col md:flex-row items-center justify-center mb-6">
+                <img onMouseEnter={(e) => bigImg(e.target)} onMouseOut={(e) => normalImg(e.target)} className="w-3/4 md:w-1/4 mb-4 md:mb-0 md:mr-48 cursor-pointer transition-transform" src={Dio4} alt=""/>
+                <p className="text-center md:text-left text-base w-full md:w-1/3">
+                    Este certificado representa minha competência com o git e github, onde aprendi a trabalhar em equipe, criar um repositório pessoal ou coletivo, fazer commits, subir meus códigos, clonar trabalhos e resolver conflitos de merge.
                 </p>
             </div>
-            <hr className="w-full border-t border-gray-300 mb-8" />
+            <hr className="w-full border-t border-gray-700 mb-6" />
 
-            <div className="flex flex-col items-center mb-8">
-                <img
-                    onMouseEnter={(e) => bigImg(e.target)}
-                    onMouseOut={(e) => normalImg(e.target)}
-                    className="w-1/4 mb-4 cursor-pointer transition-transform"
-                    src={Dio5}
-                    alt=""
-                />
-                <p className="text-center text-base">
-                    Foi neste curso onde tive meu primeiro contato com uma linguagem <br /> de programação, aprendendo atalhos que melhoram a experiência do usuário <br /> no site, um exemplo disso é essa função de dar zoom <br /> (mais perceptível em computadores) presente nos meus formulários.
+            <div className="flex flex-col md:flex-row items-center justify-center mb-6">
+                <img onMouseEnter={(e) => bigImg(e.target)} onMouseOut={(e) => normalImg(e.target)} className="w-3/4 md:w-1/4 mb-4 md:mb-0 md:mr-48 cursor-pointer transition-transform" src={Dio5} alt=""/>
+                <p className="text-center md:text-left text-base w-full md:w-1/3">
+                    Foi neste curso onde tive meu primeiro contato com uma linguagem de programação, aprendendo atalhos que melhoram a experiência do usuário no site, um exemplo disso é essa função de dar zoom (mais perceptível em computadores) presente nos meus formulários.
                 </p>
             </div>
-            <hr className="w-full border-t border-gray-300 mb-8" />
+            <hr className="w-full border-t border-gray-700 mb-6" />
 
-            <div className="flex flex-col items-center mb-8">
-                <img
-                    onMouseEnter={(e) => bigImg(e.target)}
-                    onMouseOut={(e) => normalImg(e.target)}
-                    className="w-1/4 mb-4 cursor-pointer transition-transform"
-                    src={SkillLab}
-                    alt=""
-                />
-                <p className="text-center text-base">
-                    Neste curso da <a href="https://skilllabbrasil.com.br/" className="text-blue-500 underline">Skill Lab Brasil</a> foi o mais importante que já fiz, onde aprendi <br /> muitas coisas, abrangendo todos estes cursos que acabei de descrever <br /> e mais alguns, no curso aprendi: HTML, CSS, JS, MYSQL, PHP, Laravel, Linux e outras coisas como Bootstrap, JQuery, Git, Apache, Nginx e Scrum.
+            <div className="flex flex-col md:flex-row items-center justify-center mb-6">
+                <img onMouseEnter={(e) => bigImg(e.target)} onMouseOut={(e) => normalImg(e.target)} className="w-3/4 md:w-1/4 mb-4 md:mb-0 md:mr-48 cursor-pointer transition-transform" src={SkillLab} alt=""/>
+                <p className="text-center md:text-left text-base w-full md:w-1/3">
+                    Neste curso da <a href="https://skilllabbrasil.com.br/" className="text-blue-700 underline">Skill Lab Brasil</a> foi o mais importante que já fiz, onde aprendi muitas coisas, abrangendo todos estes cursos que acabei de descrever e mais alguns, no curso aprendi: HTML, CSS, JS, MYSQL, PHP, Laravel, Linux e outras coisas como Bootstrap, JQuery, Git, Apache, Nginx e Scrum.
                 </p>
             </div>
         </main>
